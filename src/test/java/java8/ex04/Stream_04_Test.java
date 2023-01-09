eb1751b5ef18c561e2efa9f156b3d269314e312e
APdu77 <apdu77@yahoo.fr>
2023-01-09 10:38:27 +0100

    Streams

--- src/test/java/java8/ex04/Stream_04_Test.java
+++ src/test/java/java8/ex04/Stream_04_Test.java
@@ -3,6 +3,10 @@ package java8.ex04;
 
 import org.junit.Test;
 
+import java8.data.Data;
+import java8.data.domain.Customer;
+
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
@@ -16,7 +20,7 @@ public class Stream_04_Test {
     @Test
     public void test_of() throws Exception {
         // Construire un stream permettant de rendre le test passant
-        Stream<String> result = null;
+        Stream<String> result = Stream.of("Alexandra", "Cyril", "Johnny", "Marion", "Sophie");
 
         assertThat(result.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
     }
@@ -37,7 +41,7 @@ public class Stream_04_Test {
         Stream<String> s2 = Stream.of("Johnny", "Marion", "Sophie");
 
         // TODO concatener les deux streams s1 et s2
-        Stream<String> result = null;
+        Stream<String> result = Stream.concat(s1, s2);
 
         assertThat(result.toArray(), arrayContaining("Alexandra", "Cyril", "Johnny", "Marion", "Sophie"));
     }
@@ -45,8 +49,8 @@ public class Stream_04_Test {
     @Test
     public void test_iterate() throws Exception {
         // TODO utiliser la méthode "iterate" de Stream afin de rendre le test passant
-        Stream<Integer> result1 = null;
-        Stream<Integer> result2 = null;
+        Stream<Integer> result1 = Stream.iterate(1 ,i -> i);
+        Stream<Integer> result2 = Stream.iterate(1, i -> i < 6 , i -> i + 1);
         assertThat(result1.toArray(), arrayContaining(1,1,1,1,1));
         assertThat(result2.toArray(), arrayContaining(1,2,3,4,5));
     }
